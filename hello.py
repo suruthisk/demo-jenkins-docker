eccd046e58f1db4e8d769f8c53fe9a36cf3c2c44
suruthisk <95995659+suruthisk@users.noreply.github.com>
2024-03-06 16:12:35 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print('Hello, suruthi!')
+def isPalindrome(s):
+    return s == s[::-1]
+ 
+ 
+# Driver code
+s = "racecar"
+ans = isPalindrome(s)
+ 
+if ans:
+    print("Yes")
+else:
+    print("No")
